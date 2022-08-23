c2cc6a00868a35106d007ca7a813e055c246f890
Assane DIATTA <assanediatta96@gmail.com>
2022-08-23 15:42:18 +0000

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "udagramdiattadev.cqtolsa6cpzs.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
